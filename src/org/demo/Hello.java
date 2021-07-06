fb34c85f6506171532eecc4602ac8924d0483837
unknown <info.cloudline@gmail.com>
2021-07-06 14:31:10 +0530

    Folder structure

---
+++ src/org/demo/Hello.java
@@ -0,0 +1,28 @@
+package org.demo;
+
+public class Hello {
+	  public static void main(String[] args) {
+
+		  
+		  int age = 21, h=1250, w = 500;
+				  
+	if (age>18) {
+		System.out.println("Age:"+age);
+		
+		if (h!=125) {
+			System.out.println("Height:"+h);
+			
+			if (w ==50) {
+				System.out.println("Donated for blood");
+			} else {
+System.out.println("Not eligible for blood donate becoz of weight");
+			}
+		} else if (w==500) {
+System.out.println(" eligible for blood donate becoz of height");
+		}
+	} else {
+System.out.println("Not eligible for blood donate becoz of Age");
+	}
+}
+}
+
---
+++ src/org/demo/IcIcIBank.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+public class IcIcIBank {
+
+public static void main(String[] args) {
+
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.exit(0);
+		System.out.println(6);
+		System.out.println(7);
+}
+}
